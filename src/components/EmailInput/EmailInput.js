c8b96eef81598fd902e7ed04672034426ae9c6ca
Morgan Bonar <mbonar@gmail.com>
2017-03-15 21:18:42 -0700

    more tests. adding onChange events for validation

--- src/components/EmailInput/EmailInput.js
+++ src/components/EmailInput/EmailInput.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 class EmailInput extends React.Component {
-	validateMe() {
-		
+	validateMe(e) {
+		e.preventDefault();
+		console.log(e.target.value)
 	}
 
 	render() {
 		return (
 			<div className="input-container email-input-container">
-	      <label className="email-input-label" htmlFor="email-input--email">
-	          Email <b className="required">*</b>
-	      </label>
-	      <div className="email-input">
-	          <input id="email-input--email" name="email" required="required" className="nsg-form--input text-input" data-validator="email" data-error="Please enter a valid email address." defaultValue="mbonar@gmail.com" data-has-validation="true" />
-	      </div>
-      </div> 
+				<label className="email-input-label" htmlFor="email-input--email">
+					Email <b className="required">*</b>
+				</label>
+				<div className="email-input">
+					<input 
+						onChange={this.validateMe} 
+						id="email-input--email" 
+						name="email" 
+						required="required" 
+						className="nsg-form--input text-input" 
+						data-validator="email" 
+						data-error="Please enter a valid email address." 
+						defaultValue="mbonar@gmail.com" 
+						data-has-validation="true" 
+					/>
+				</div>
+			</div> 
 		)
 	}
 }
